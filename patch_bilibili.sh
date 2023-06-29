25d2f5f2312e249037926d0d087fb4ccecd11315
hmgdd101 <51875934+hmgdd101@users.noreply.github.com>
2023-06-29 12:52:24 +0800

    Update patch_bilibili.sh

--- patch_bilibili.sh
+++ patch_bilibili.sh
@@ -4,7 +4,7 @@
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     # 下载游戏apk的地址,我找不到一个固定的链接,理论上每次更新客户端都要手动改地址
-    url="https://objects.githubusercontent.com/github-production-release-asset-2e65be/659149393/3dc07b58-a6f5-4872-be70-98a114a0800b?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230629%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230629T043708Z&X-Amz-Expires=300&X-Amz-Signature=44131670e6ba0cb8b37dd9a47043d3343f26aaf777b2a2df4ba08ae355fb7dee&X-Amz-SignedHeaders=host&actor_id=51875934&key_id=0&repo_id=659149393&response-content-disposition=attachment%3B%20filename%3Dblhx_7.1.1_20230610_1_20230616_031339_073b2.apk&response-content-type=application%2Fvnd.android.package-archive"
+    url="https://github.com/hmgdd101/azurlane/releases/download/default/blhx_7.1.1_20230610_1_20230616_031339_073b2.apk"
     # 使用wget命令下载apk文件
     curl -o blhx.apk  $url
     fi
